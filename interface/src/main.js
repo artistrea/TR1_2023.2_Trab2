516029f195fde674be8e9d22e2acd92fa3ed00f3
artistrea <apiravesi@gmail.com>
2023-11-08 00:22:49 -0300

    feat: interface observing transmitter done

--- interface/src/main.js
+++ interface/src/main.js
@@ -1,54 +1,23 @@
-const indicadorConexaoTransmissor = document.querySelector(
-  "#indicador-conexao-transmissor"
-);
+import { subscribe } from "./subscribe";
 
-// Subscribe to the event source at `uri` with exponential backoff reconnect.
-function subscribe(uri, name) {
-  // name: transmitter | receptor
-  var retryTime = 1;
+const transmitterBaseUrl = "http://localhost:3001";
 
-  function connect(uri) {
-    const events = new EventSource(uri);
+subscribe(`${transmitterBaseUrl}/subscribe-interface`, "transmitter");
+// subscribe("http://localhost:3002/subscribe-interface", "receptor");
 
-    events.addEventListener("message", (ev) => {
-      console.log("raw data", JSON.stringify(ev.data));
-      console.log("decoded data", JSON.stringify(JSON.parse(ev.data)));
-      const msg = JSON.parse(ev.data);
-      if (!"message" in msg || !"room" in msg || !"username" in msg) return;
-      addMessage(msg.room, msg.username, msg.message, true);
-    });
+const textInput = document.querySelector("input#text-to-send");
 
-    events.addEventListener("open", () => {
-      setConnectedStatus(name, true);
-      console.log(`connected to event stream at ${uri}`);
-      retryTime = 1;
-    });
+document.querySelector("form#send-text-form").onsubmit = (e) => {
+  e.preventDefault();
+  fetch(`${transmitterBaseUrl}/`, {
+    method: "POST",
+    body: JSON.stringify({ text: textInput.value }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
 
-    events.addEventListener("error", () => {
-      setConnectedStatus(name, false);
-      events.close();
-
-      let timeout = retryTime;
-      retryTime = Math.min(64, retryTime * 2);
-      console.log(`connection lost. attempting to reconnect in ${timeout}s`);
-      setTimerValue(name, timeout);
-      setTimeout(() => connect(uri), (() => timeout * 1000)());
-    });
-  }
-
-  connect(uri);
-}
-
-function setTimerValue(name, value) {
-  document
-    .querySelector(`#${name}-connected-status`)
-    .querySelector("[data-name='timer']").innerHTML = value;
-}
-
-function setConnectedStatus(name, isConnected) {
-  // name: transmitter | receptor
-  document
-    .querySelector(`#${name}-connected-status`)
-    .setAttribute("data-connected", isConnected);
-}
-// subscribe("/events", "transmitter");
+    // Displaying results to console
+    .then((json) => console.log(json));
+};
--- transmitter/server.ts
+++ transmitter/server.ts
@@ -1,12 +1,65 @@
 import express from "express";
+import cors from "cors";
 import type { Response } from "express-serve-static-core";
 
 const port = 3001;
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 let interfaceClient: Response<any, Record<string, any>, number> | undefined;
-let receptor: Response<any, Record<string, any>, number> | undefined;
+
+type InterfaceMessage =
+  | {
+      type: "connected";
+    }
+  | {
+      type: "text";
+      content: string;
+    }
+  | {
+      type: "bits";
+      content: string;
+    };
+
+function encode(data: string) {
+  let out = "";
+
+  for (let i = 0; i < data.length; i++) {
+    let num = data.charCodeAt(0);
+    let charBits = "";
+    let j = 8;
+    while (j--) {
+      if (num & 1) charBits = "1" + charBits;
+      else charBits = "0" + charBits;
+      num >>= 1;
+    }
+    out += charBits;
+  }
+
+  return out;
+}
+
+app.post("/", (req, res) => {
+  const { text } = req.body;
+
+  if (typeof text !== "string") {
+    res.status(400).send({ message: "text sent should be string" });
+    return;
+  }
+  if (interfaceClient) sendDataToInterface({ type: "text", content: text });
+
+  const encodedData = encode(text);
+
+  if (interfaceClient)
+    sendDataToInterface({ type: "bits", content: encodedData });
+
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+  };
+  res.status(200).header(headers).send({ message: "yay" });
+});
 
 app.get("/subscribe-interface", (request, response) => {
   const headers = {
@@ -14,17 +67,17 @@ app.get("/subscribe-interface", (request, response) => {
     Connection: "keep-alive",
     "Cache-Control": "no-cache",
   };
+
   response.writeHead(200, headers);
-
-  const data = `data: ${JSON.stringify("opa")}\n\n`;
-
-  response.write(data);
+  response.flushHeaders();
 
   if (interfaceClient) {
     interfaceClient.end();
     console.warn("A interfaceClient has been forcefully disconnected");
   }
   interfaceClient = response;
+  console.log("Client connected");
+  sendDataToInterface({ type: "connected" });
 
   // lost connection:
   request.on("close", () => {
@@ -32,6 +85,10 @@ app.get("/subscribe-interface", (request, response) => {
     console.log("Client disconnected");
   });
 });
+
+function sendDataToInterface(data: InterfaceMessage) {
+  interfaceClient?.write(`data: ${JSON.stringify(data)}\n\n`);
+}
 
 app.listen(port, () => {
   console.log(`Transmitter app listening on http://localhost:${port}`);
