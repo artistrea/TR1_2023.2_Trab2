a74cfc6128ccc53c37428061c87473670f00c2cf
artistrea <apiravesi@gmail.com>
2023-11-27 11:33:54 -0300

    refactor: chart variable names

--- interface/src/chart.js
+++ interface/src/chart.js
@@ -1,17 +1,7 @@
 import Plotly from "plotly.js-dist-min";
 
-const graphDiv = document.getElementById("graph");
+const chartDiv = document.getElementById("chart");
 
-function cossine(precision, freq=1) {
-  const xt = [];
-  const yt = [];
-  for (let i = 0; i < precision; i++) {
-    xt.push(i / precision);
-    yt.push(Math.cos((i * 2 * Math.PI * freq) / precision));
-  }
-
-  return { xt, yt };
-}
 
 function sine(freq=1, fase=0, precision=300) {
   const xt = [];
@@ -23,7 +13,7 @@ function sine(freq=1, fase=0, precision=300) {
   return { xt, yt };
 }
 
-function modulation(graphBits = "101100001000010011110111", graphType="8QAM") {
+function modulation(chartBits, chartType) {
   const { xt, yt } = sine();
   const {xt: xt_2f, yt: yt_2f} = sine(2);
   const {xt: xt_p_PI0_5, yt: yt_p_PI0_5} = sine(1,Math.PI*0.5);
@@ -31,24 +21,24 @@ function modulation(graphBits = "101100001000010011110111", graphType="8QAM") {
   const {xt: xt_p_PI1_5, yt: yt_p_PI1_5} = sine(1,Math.PI*1.5);
   const x = []
   const y = []
-  switch(graphType) {
+  switch(chartType) {
     case("ASK"):
-    for (let i = 0; i < graphBits.length; i++) {
+    for (let i = 0; i < chartBits.length; i++) {
       x.push(...xt.map((x)=>x+i))
-      y.push(...yt.map((y)=> graphBits[i] === '1' ? y : y))
+      y.push(...yt.map((y)=> chartBits[i] === '1' ? y : y))
     }
     break;
     case("FSK"):
-    for (let i = 0; i < graphBits.length; i++) {
-        x.push(...(graphBits[i] === '1' ? xt_2f : xt).map((x)=>x+i))
-        y.push(...(graphBits[i] === '1' ? yt_2f : yt))
+    for (let i = 0; i < chartBits.length; i++) {
+        x.push(...(chartBits[i] === '1' ? xt_2f : xt).map((x)=>x+i))
+        y.push(...(chartBits[i] === '1' ? yt_2f : yt))
       }
     break;
     case("8QAM"):
-      while(graphBits.length % 3 != 0) graphBits += 0 // garantir que temos simbolos completos
-      for (let i = 0; i < graphBits.length; i+=3) {
+      while(chartBits.length % 3 != 0) chartBits += 0 // garantir que temos simbolos completos
+      for (let i = 0; i < chartBits.length; i+=3) {
         let j = i/3;
-        switch(graphBits.slice(i,i+3)){
+        switch(chartBits.slice(i,i+3)){
           case("000"):
             x.push(...xt.map((x)=>x+j))
             y.push(...yt.map((y)=>y/2))
@@ -91,29 +81,29 @@ function modulation(graphBits = "101100001000010011110111", graphType="8QAM") {
   return { x, y }
 }
 
-let graphBits = "10110001";
-let graphType = "8QAM";
+let chartBits = "10110001";
+let chartType = "8QAM";
 
 /**
  * @param {string} data - bit sequence in string
  */
-export function setGraphBits(data) {
-  graphBits = data;
+export function setChartBits(data) {
+  chartBits = data;
   replot();
 }
 
 /**
  * @param {"ASK" | "FSK" | "8QAM"} type
  */
-export function setGraphType(type) {
-  graphType = type;
+export function setChartType(type) {
+  chartType = type;
   replot();
 }
 
 function replot() {
-  const {x, y} = modulation()
+  const {x, y} = modulation(chartBits ,chartType)
   Plotly.newPlot(
-    graphDiv,
+    chartDiv,
     [
       {
         x,
