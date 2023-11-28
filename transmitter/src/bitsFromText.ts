a2f2fc6e3d7405712a9069744b7121b66a3168c8
artistrea <apiravesi@gmail.com>
2023-11-27 21:50:21 -0300

    cleaner bitsFromText

--- transmitter/src/bitsFromText.ts
+++ transmitter/src/bitsFromText.ts
@@ -3,13 +3,8 @@ export function bitsFromText(data: string) {
 
   for (let i = 0; i < data.length; i++) {
     let charAscii = data.charCodeAt(i);
-    let charBits = "";
-    let j = 8;
-    while (j--) {
-      if (charAscii & 1) charBits = "1" + charBits;
-      else charBits = "0" + charBits;
-      charAscii >>= 1;
-    }
+    let charBits = charAscii.toString(2);
+
     bits += charBits;
   }
 
