858771491633ffa83acb572487b65ffc6a803df2
artistrea <apiravesi@gmail.com>
2023-11-27 21:46:00 -0300

    hotfix: textFromBits

--- receptor/src/textFromBits.ts
+++ receptor/src/textFromBits.ts
@@ -2,7 +2,7 @@ export function textFromBits(data: string) {
   let text = "";
 
   for (let i = 0; i < data.length; i += 8) {
-    text += parseInt(data.slice(i, i + 8), 2);
+    text += String.fromCharCode(parseInt(data.slice(i, i + 8), 2));
   }
 
   return text;
