da0a59d4058f8bc26718362ba7933554e9ca1bb0
artistrea <apiravesi@gmail.com>
2023-12-10 11:55:50 -0300

    refactor checkBitCount & added checkCharCount

--- receptor/src/checkBitCounting.ts
+++ receptor/src/checkBitCounting.ts
@@ -1,41 +1,40 @@
-export function checkBitCount(data: string): string {
-    let bits = data;
-    let checkHeader = 0;
-    let bitsSaida = ""
+import { frameSize, headerSize } from "./config";
 
-    for(let i = 0; i>= 0; i += 71) {
-        bits = data.slice(i+7, i+71);
-        checkHeader = parseInt(data.slice(i, i+7), 2);
-        if(checkHeader != bits.length) {
-            throw("Deu erro com header, há interferência")
-        }
-        bitsSaida = bitsSaida.concat(bits);
-        if(i+71 >= data.length) {
-            return bitsSaida
-        }
+const frameWHeaderSize = frameSize + headerSize;
+
+function checkCount(data: string, headerMultiplier: number) {
+  let bits = data;
+  let checkHeader = 0;
+  let bitsSaida = "";
+
+  for (let i = 0; i >= 0; i += frameWHeaderSize) {
+    bits = data.slice(i + headerSize, i + frameWHeaderSize);
+    checkHeader = parseInt(data.slice(i, i + headerSize), 2);
+
+    if (checkHeader * headerMultiplier < bits.length) {
+      throw "Deu erro com header, há interferência";
     }
 
-    let frameSize = 64;
-    return "Erro na função a qual faz a retiragem e checagem dos quadros";
+    bitsSaida = bitsSaida.concat(bits);
+    if (i + frameWHeaderSize >= data.length) {
+      return bitsSaida;
+    }
+  }
+
+  throw "Erro na função a qual faz a retiragem e checagem dos quadros";
 }
 
+// throws if error in frame
+export function checkBitCount(data: string): string {
+  return checkCount(data, 1);
+}
+
+// throws if error in frame
 export function checkWordCount(data: string): string {
-    let bits = data;
-    let checkHeader = 0;
-    let bitsSaida = ""
+  return checkCount(data, 32);
+}
 
-    for(let i = 0; i>= 0; i += 71) {
-        bits = data.slice(i+7, i+71);
-        checkHeader = parseInt(data.slice(i, i+7), 2);
-        if(checkHeader*32 < bits.length) {
-            throw("Deu erro com header, há interferência")
-        }
-        bitsSaida = bitsSaida.concat(bits);
-        if(i+71 >= data.length) {
-            return bitsSaida
-        }
-    }
-
-    let frameSize = 64;
-    return "Erro na função a qual faz a retiragem e checagem dos quadros";
+// throws if error in frame
+export function checkCharCount(data: string): string {
+  return checkCount(data, 8);
 }
