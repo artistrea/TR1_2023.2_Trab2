1faf5cacea0b270c92850c9673c740bf0dec2bbc
LaKHamote <lucascboa@gmail.com>
2023-12-10 23:34:46 -0300

    💡

--- transmitter/src/config.ts
+++ transmitter/src/config.ts
@@ -3,11 +3,10 @@ export const frameSize = {
   hamming: 34,
   paridade: 63,
 };
-
-export const EDC: keyof typeof frameSize = "hamming";
-
 export const headerSize = 7;
 export const flag = "11110000";
 export const esc = "11111111";
 
-export const frameLimiter: "count" | "insertion" = "insertion";
+// valores alteráveis a gosto
+export const EDC: keyof typeof frameSize = "hamming";
+export const frameLimiter: "count" | "insertion" = "count";
