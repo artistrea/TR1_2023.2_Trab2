13d2f69db3b221b6607cbf264cfc9eb68d751149
LaKHamote <lucascboa@gmail.com>
2023-12-10 23:45:43 -0300

    💡

--- receptor/src/config.ts
+++ receptor/src/config.ts
@@ -7,7 +7,6 @@ export const headerSize = 7;
 export const flag = "11110000";
 export const esc = "11111111";
 
-
 // valores alteráveis a gosto
 export const EDC: keyof typeof frameSize = "hamming";
 export const frameLimiter: "count" | "insertion" = "count";
